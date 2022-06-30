da165ddf62a132662e31466a9005b59470605be8
Noah White <njwhite@umass.edu>
2022-06-30 12:18:17 -0400

    implimented TODO 1,2,3 of part 1

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import chalkAnimation from 'chalk-animation';
 import { database } from './database.js';
 // TODO #1: Import Morgan and Express
-
+import express from 'express';
+import logger from 'morgan';
 // TODO #2: Create an Express app.
-
+const app = express();
+const port = process.env.PORT || 3000;
 // TODO #3: Add middleware to the Express app.
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use('/', express.static('client'));
 // TODO #4: Implement the /wordScore endpoint
 
 // TODO #5: Implement the /highestWordScores endpoint
